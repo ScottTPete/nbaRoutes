c6ec51fffcc35dd47efdb0ad9a5608487ce46ce7
Scott Peterson <stpeterson28@yahoo.com>
2016-04-11 16:19:03 -0600

    Forgot to push the completed project

--- js/teams/teamService.js
+++ js/teams/teamService.js
@@ -2,6 +2,47 @@ var app = angular.module('nbaRoutes');
 
 app.service('teamService', function ($http, $q) {
 
-    // service code
+    this.addNewGame = function(gameObject) {
+		
+		var url = 'https://api.parse.com/1/classes/' + 
+		gameObject.homeTeam;
+		
+		if(parseInt(gameObject.homeTeamScore) > parseInt(gameObject.opponentScore)) {
+			gameObject.won = true;
+		} else {
+			gameObject.won = false;
+		}
+		return $http ({
+			method: 'POST',
+			url: url,
+			data: gameObject
+		});
+	};
+	
+	this.getTeamData = function(team) {
+		var defer = $q.defer();
+		var url =  'https://api.parse.com/1/classes/' + team;
+		
+		$http ({
+			method: 'GET',
+			url: url,
+		}).then(function(data){
+			var results = data.data.results;
+			var wins = 0;
+			var losses = 0;
+			for(var i = 0; i < results.length; i++) {
+				if(results[i].won === true) {
+					wins++;
+				} else {
+					losses++;
+				}
+			}
+			console.log(results);
+			results.wins = wins;
+			results.losses = losses;
+			defer.resolve(results);
+		})
+		return defer.promise;
+	}
 
 });
